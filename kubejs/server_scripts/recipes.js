a528b24e032c3cc32f77562a1e85231d898384f6
ZoesDev <6062364+ZoesDev@users.noreply.github.com>
2022-05-25 21:40:16 -0700

    added some of the custom recipies. will need to add more and remove some others

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -13,14 +13,49 @@ onEvent('recipes', event => {
         P: 'solarflux:sp_7',
         E: 'minecraft:dragon_egg'
 
-    });
+    })
 
-    //a few misc crafts to make things easier
+    //a few misc crafts to make things easier. see I am not fully a masochist
     event.shapeless('minecraft:flint', [
         'SS',
         'S'
       ], {
         S: 'minecraft:gravel'
-      });
+      })
+    event.shapeless('minecraft:red_mushroom_block', [
+        'SS',
+        'SS'
+      ], {
+        S: 'minecraft:red_mushroom'
+      })
+    event.shapeless('minecraft:brown_mushroom_block', [
+        'SS',
+        'SS'
+      ], {
+        S: 'minecraft:brown_mushroom'
+      })
+    event.shaped('minecraft:cobweb', [
+        'SSS',
+        'SSS',
+        'SSS'
+      ], {
+        S: 'minecraft:string'
+      })
+    event.shapeless(item.of('minecraft:string', 4), [
+        'S'
+      ], {
+        S: '#minecraft:wool'
+      })
+    event.shapeless(item.of('minecraft:melon_slice', 9), [
+        'S'
+      ], {
+        S: 'minecraft:melon'
+      })
+    event.shapeless(item.of('minecraft:quartz', 4), [
+        'S'
+      ], {
+        S: 'minecraft:quartz_block'
+      })
+
 
   })
