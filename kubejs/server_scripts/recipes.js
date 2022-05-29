1c64af0bc9c4db0f0d4bae67aa08e0772c7ba5cf
ZoesDev <6062364+ZoesDev@users.noreply.github.com>
2022-05-28 19:16:09 -0700

    added fristlogin script to kubejs to start editing for adding book, setup a few mods to be better

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -2,7 +2,7 @@ onEvent('recipes', event => {
 	event.remove({output: 'projecte:transmutation_table'})
 
     //projecte Addition
-    event.shaped('projecte:transmutation_table', [
+  event.shaped('projecte:transmutation_table', [
         'NPN',
         'SES',
         'NKN'
@@ -16,46 +16,15 @@ onEvent('recipes', event => {
     })
 
     //a few misc crafts to make things easier. see I am not fully a masochist
-    event.shapeless('minecraft:flint', [
+  event.shapeless('minecraft:flint', [
         'SS',
         'S'
       ], {
         S: 'minecraft:gravel'
       })
-    event.shapeless('minecraft:red_mushroom_block', [
-        'SS',
-        'SS'
-      ], {
-        S: 'minecraft:red_mushroom'
-      })
-    event.shapeless('minecraft:brown_mushroom_block', [
-        'SS',
-        'SS'
-      ], {
-        S: 'minecraft:brown_mushroom'
-      })
-    event.shaped('minecraft:cobweb', [
-        'SSS',
-        'SSS',
-        'SSS'
-      ], {
-        S: 'minecraft:string'
-      })
-    event.shapeless(item.of('minecraft:string', 4), [
+  event.shapeless(item.of('minecraft:string', 4), [
         'S'
       ], {
         S: '#minecraft:wool'
       })
-    event.shapeless(item.of('minecraft:melon_slice', 9), [
-        'S'
-      ], {
-        S: 'minecraft:melon'
-      })
-    event.shapeless(item.of('minecraft:quartz', 4), [
-        'S'
-      ], {
-        S: 'minecraft:quartz_block'
-      })
-
-
   })
--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -18,3 +18,14 @@ onEvent('item.tags', event => {
 })
 
 
+onEvent('player.logged_in', event => {
+	// Check if player doesn't have "starting_items" stage yet
+	if (!event.player.stages.has('starting_items')) {
+	  // Add the stage
+	  event.player.stages.add('starting_items')
+	  // Give some items to player
+	  event.player.give('minecraft:stone_sword')
+	  event.player.give(Item.of('minecraft:stone_pickaxe', "{Damage: 10}"))
+	  event.player.give('30x minecraft:apple')
+	}
+  })
