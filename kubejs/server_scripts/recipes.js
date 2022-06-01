14b7aa18f9eeb6a88a405162a14b5cf5faaec914
Zoe <zoe@DESKTOP-KO32JBR.localdomain>
2022-06-01 14:03:57 -0700

    changes to configs. updated mods, cleaned up mods

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -2,29 +2,29 @@ onEvent('recipes', event => {
 	event.remove({output: 'projecte:transmutation_table'})
 
     //projecte Addition
-  event.shaped('projecte:transmutation_table', [
-        'NPN',
-        'SES',
-        'NKN'
-    ], {
-        K: 'projecte:klein_star_ein',
-        S: 'extendedcrafting:ender_star',
-        N: 'minecraft:netherite_block',
-        P: 'solarflux:sp_7',
-        E: 'minecraft:dragon_egg'
+  // event.shaped('projecte:transmutation_table', [
+  //       'NPN',
+  //       'SES',
+  //       'NKN'
+  //   ], {
+  //       K: 'projecte:klein_star_ein',
+  //       S: 'extendedcrafting:ender_star',
+  //       N: 'minecraft:netherite_block',
+  //       P: 'solarflux:sp_7',
+  //       E: 'minecraft:dragon_egg'
 
-    })
+  //   })
 
-    //a few misc crafts to make things easier. see I am not fully a masochist
-  event.shapeless('minecraft:flint', [
-        'SS',
-        'S'
-      ], {
-        S: 'minecraft:gravel'
-      })
-  event.shapeless(item.of('minecraft:string', 4), [
-        'S'
-      ], {
-        S: '#minecraft:wool'
-      })
+  //   //a few misc crafts to make things easier. see I am not fully a masochist
+  // event.shapeless('minecraft:flint', [
+  //       'SS',
+  //       'S'
+  //     ], {
+  //       S: 'minecraft:gravel'
+  //     })
+  // event.shapeless(item.of('minecraft:string', 4), [
+  //       'S'
+  //     ], {
+  //       S: '#minecraft:wool'
+  //     })
   })
