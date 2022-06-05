c86e3b6bd0be7fd5a11d28639b17480900a18932
Zoe <zoe@endofmine.com>
2022-06-05 09:22:36 -0700

    updated readme basic recipies

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,19 +1,54 @@
 onEvent('recipes', event => {
 	event.remove({output: 'projecte:transmutation_table'})
 
-    //projecte Addition
-  // event.shaped('projecte:transmutation_table', [
-  //       'NPN',
-  //       'SES',
-  //       'NKN'
-  //   ], {
-  //       K: 'projecte:klein_star_ein',
-  //       S: 'extendedcrafting:ender_star',
-  //       N: 'minecraft:netherite_block',
-  //       P: 'solarflux:sp_7',
-  //       E: 'minecraft:dragon_egg'
 
-  //   })
+
+
+
+
+
+  //projecte Addition
+// Example of using extended crafting shaped table
+  //   event.recipes.extendedcrafting.shaped_table('projecte:transmutation_table', [
+//     'KKKKKKKKK',
+//     'KKKKKKKKK',
+//     'KKKKKKKKK',
+//     'KKKKKKKKK',
+//     'KKKKKKKKK',
+//     'KKKKKKKKK',
+//     'KKKKKKKKK',
+//     'KKKNKKKKK',
+//     'KKKKKKKKE'
+// ], {
+//     K: 'projecte:klein_star_ein',
+//     N: 'minecraft:netherite_block',
+//     E: 'minecraft:dragon_egg'
+// })
+
+
+event.recipes.extendedcrafting.shaped_table('projecte:transmutation_table', [
+  'KKKKKKKKK',
+  'KKKKKKKKK',
+  'KKKKKKKKK',
+  'KKKKKKKKK',
+  'KKKKKKKKK',
+  'KKKKKKKKK',
+  'KKKKKKKKK',
+  'KKKNKKKKK',
+  'KKKKKKKKE'
+], {
+  K: 'projecte:klein_star_ein',
+  N: 'minecraft:netherite_block',
+  E: 'minecraft:dragon_egg'
+
+})
+
+
+
+
+
+
+
 
   //   //a few misc crafts to make things easier. see I am not fully a masochist
   // event.shapeless('minecraft:flint', [
